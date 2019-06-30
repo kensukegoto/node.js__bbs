9645d4c85082a1597e3a939b8c548c273fd68709
kensukegoto <utogoken2@yahoo.co.jp>
2019-06-30 14:08:42 +0900

    ルーターに / post と /get を追加

--- server/route/route.js
+++ server/route/route.js
@@ -1,15 +1,28 @@
 const router = require('express').Router();
 
 var data = [];
-router.post('/', function (req, res) {
+
+/**
+ * リクエストを受け付けてDBに登録
+ * DBからデータを取得し返す
+ */
+router.post('/post', function (req, res) {
   console.log(req);
   data.push("hello");
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
   res.send({data:data});
 })
-router.get('/', function (req, res) {
+
+/**
+ * DBからデータを取得し返す
+ * data配列に基本的には溜まっているので無い分だけ取得
+ */
+router.get('/get', function (req, res) {
   res.setHeader('Content-Type', 'application/json; charset=utf-8')
   res.send({data:data});
 })
+
+
+
 
 module.exports = router;
